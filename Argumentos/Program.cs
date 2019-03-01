4b907590034409336a8b63ad66a000d7aaa409f8
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-01 12:47:13 +0000

    Doc comment

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,8 +2,15 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// This program prints out the arguments given by the program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This is the main function with the arguments.
+        /// </summary>
+        /// <param name="args"> Array de strings</param>
         static void Main(string[] args)
         {
             for (int i = 0; i < args.Length; i++)
