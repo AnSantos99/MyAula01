901b706d76a5d6f71ad5888bb35077c90769af7a
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-04 23:01:27 +0000

    Ex4 TiposVarios

---
+++ TiposVarios/TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Integers of diferent types.
+            int num_inteiro = 4;
+            uint unsign_int = 4U;
+            long long_int = 4L;
+            ulong long_unsignint = 4UL;
+
+            Console.WriteLine(num_inteiro);
+            Console.WriteLine(unsign_int);
+        }
+    }
+}
