9103cddf7bfe3017b4b722a4d54f75d8e5d0d50f
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-05 21:50:40 +0000

    Update of TiposVarios

--- TiposVarios/TiposVarios/Program.cs
+++ TiposVarios/TiposVarios/Program.cs
@@ -16,21 +16,17 @@ namespace TiposVarios
             long long_int = 134L;
             ulong long_unsignint = 11UL;
 
+            //Char variables using unicode values
             char yen_sign = '\u00A5';
             char registered_sign = '\u00AE';
             char cyrillic_letterZhe = '\u0416';
             char letter_phi = '\u03A6';
 
+
+
             //Variables output
-            Console.WriteLine(num_inteiro);
-            Console.WriteLine(unsign_int);
-            Console.WriteLine(long_int);
-            Console.WriteLine(long_unsignint);
-            Console.WriteLine();
-            Console.WriteLine(yen_sign);
-            Console.WriteLine(registered_sign);
-            Console.WriteLine(cyrillic_letterZhe);
-            Console.WriteLine(letter_phi);
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n ", num_inteiro, unsign_int, long_int, long_unsignint);
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n", yen_sign, registered_sign, cyrillic_letterZhe, letter_phi);
         }
     }
 }
